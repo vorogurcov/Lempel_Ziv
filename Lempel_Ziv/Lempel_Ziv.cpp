54a2fe0c83840265757ec559c80d26cc10b95741
vorogurcov <vania.frol2010@yandex.ru>
2024-09-29 13:31:59 +0300

    Implement Lempel_Ziv prototype. Implement FindIndex() and Length() interfaces.

--- Lempel_Ziv/Lempel_Ziv.cpp
+++ Lempel_Ziv/Lempel_Ziv.cpp
@@ -1,20 +1,48 @@
-// Lempel_Ziv.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Information
+{
+    vector<pair<int, char>>* code;
+    vector<string>* dictionary;
+};
+
+int FindIndex(vector<char>::iterator it)
+{
+
+}
+
+int Length(int index)
+{
+
+}
+
+Information& Lempel_Ziv(vector<char>& vec)
+{
+    Information* inf = new Information;
+    vector<pair<int, char>>* code = new vector<pair<int,char>>;
+    vector<string>* dict = new vector<string>; 
+    vector<char>::iterator it;
+
+    for (it = vec.begin() ;it  != vec.end(); )
+    {
+        int curindex = FindIndex(it);
+        int length = Length(curindex);
+        code->push_back(make_pair(curindex, *it));
+        dict->push_back((*dict)[curindex] + *(it + length));
+        it += length + 1;
+    }
+
+    inf->code = code;
+    inf->dictionary = dict;
+    return *inf;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
