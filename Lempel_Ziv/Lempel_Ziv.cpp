1513927df31bcbbfaff38b334cb484956a7b502e
vorogurcov <vania.frol2010@yandex.ru>
2024-10-06 11:48:13 +0300

    Implement last changes with .bin file reading. Resolve some problems with streams

--- Lempel_Ziv/Lempel_Ziv.cpp
+++ Lempel_Ziv/Lempel_Ziv.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 struct Information
 {
-    vector<pair<int, char>>* code;
+    vector<pair<unsigned int, char>>* code;
     vector<string>* dictionary;
 };
 
@@ -40,14 +40,14 @@ int FindIndex(vector<char>::iterator iter,vector<string>& dict,int index, vector
 Information& Lempel_Ziv(vector<char>& vec)
 {
     Information* inf = new Information;
-    vector<pair<int, char>>* code = new vector<pair<int,char>>;
+    vector<pair<unsigned int, char>>* code = new vector<pair<unsigned int,char>>;
     vector<string>* dict = new vector<string>; 
     (*dict).push_back("");
 
     for (vector<char>::iterator it = vec.begin() ;it  != vec.end(); )
     {
-        int curindex = FindIndex(it,*dict,dict->size(),vec.end());
-        int length = (*dict)[curindex].length();
+        unsigned int curindex = FindIndex(it,*dict,dict->size(),vec.end());
+        unsigned int length = (*dict)[curindex].length();
         code->push_back(make_pair(curindex, (length == 0)?*it:*(it+length)));
         dict->push_back((*dict)[curindex] + *(it + length));
         it += length + 1;
@@ -58,11 +58,11 @@ Information& Lempel_Ziv(vector<char>& vec)
     return *inf;
 }
 
-void decode_Lempel_Ziv(ofstream& ofs,vector<pair<int, char>>* code)
+void decode_Lempel_Ziv(ofstream& ofs,vector<pair<unsigned int, char>>* code)
 {
     vector<string>* dict = new vector<string>;
     dict->push_back("");
-    for (vector<pair<int, char>>::iterator iter = code->begin(); iter != code->end(); iter++)
+    for (vector<pair<unsigned int, char>>::iterator iter = code->begin(); iter != code->end(); iter++)
     {
         int p = (*iter).first;
         char q = (*iter).second;
@@ -79,12 +79,13 @@ void printvec(const vector<string>& vec)
         cout << el << endl;
     }
 }
-void writepairs(ofstream& ofs,Information& inf)
+void writepairs(fstream& ofs,Information& inf)
 {
-    vector<pair<int, char>>& vec = *inf.code;
+    vector<pair<unsigned int, char>>& vec = *inf.code;
     for (auto& el : vec)
     {
-        ofs << el.first << el.second << endl;
+        ofs.write(reinterpret_cast<char*>(&el.first), sizeof(unsigned int));
+        ofs.write(&el.second, sizeof(char));
     }
 }
 int main()
@@ -121,9 +122,10 @@ int main()
 
         obj = Lempel_Ziv(vec);
         cout << "Success! Compressed version of " << filename << ".txt takes only \n";
-        cout << obj.code->size() * 3 << " bytes!";
+        cout << obj.code->size()*(sizeof(unsigned int) + sizeof(char))  << " bytes!";
         printvec(*obj.dictionary);
-        ofstream ofs("compressed_data.txt");
+        fstream ofs("compressed_data.dat", ios::binary | ios::out);
+        //TODO: Implement writebin
         writepairs(ofs, obj);
     }
     else
@@ -131,37 +133,19 @@ int main()
         cout << "Type in the name of the file which you need to decompress \n";
         cin >> filename;
         
-        ifstream ifs(filename + ".txt");
-        vector<pair<int, char>> code;
+        fstream ifs(filename + ".dat", ios::binary | ios::in);
+        vector<pair<unsigned int, char>> code;
         if (ifs.is_open())
         {
             while (!ifs.eof())
             {
-                string data;
-                //getline(ifs, data);
-                char symb;
-                int d = 0;
+                unsigned int d;
                 char sym;
-                while (ifs.get(symb))
-                {
-                    
-                    if (isdigit(symb))
-                    {
-                        data.push_back(symb);
-                    }
-                    else
-                    {
-                        sym = symb;
-                        break;
-                    }
-                }
-                    
-                if (data == "")
-                    continue;
-                d = stoi(data);
+                ifs.read(reinterpret_cast<char*>(&d), sizeof(unsigned int));
+                ifs.read(&sym, sizeof(char));
                 code.push_back(make_pair(d,sym));
             }
-            
+            code.pop_back();
             ifs.close();
         }
         else
